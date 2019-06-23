140f924788760172f9e9f616086604ae53bb0ae2
ZelleM <groovyrlm@gmail.com>
2019-06-22 21:57:10 -0400

    Spotify should be working. Not sure why it isn't. Moving on for now.

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//requires
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
@@ -5,43 +6,54 @@ const axios = require('axios');
 var moment = require('moment');
 moment().format();
 
+//process spotify requests
 var spotify = new Spotify(keys.spotify);
- 
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- console.log(data); 
-});
-
+var getArtistsNames = function(artist) {
+    return artist.name;
+}
+var getSong = function(songName) {
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        songs = data.tracks.items; 
+        for (var i = 0; i = songs.length; i++) {
+            console.log(i);
+            console.log("artist(s): " + songs[i].artists.map(getArtistsNames));
+            console.log("song name: " + songs[i].name);
+            console.log("preview song: " + songs[i].preview_url);
+            console.log("album name: " + songs[i].album.name);
+            console.log("----------------------------------------");
+        } //end for
+    }); //end search
+} // end getSong
  
 // Make a request for a user with a given ID
 axios.get('/user?ID=12345')
-  .then(function (response) {
+    .then(function (response) {
     // handle success
     console.log(response);
-  })
-  .catch(function (error) {
+    })
+    .catch(function (error) {
     // handle error
     console.log(error);
-  })
-  .finally(function () {
+    })
+    .finally(function () {
     // always executed
-  });
+});
 
-var doIt = function () {
-    switch() {
-        case concert-this:
+var doIt = function (caseChosen, dataPassed) {
+    switch(caseChosen) {
+        case 'concert-this':
             //code
             break;
-        case spotify-this-song:
+        case 'spotify-this-song':
+            getSong(dataPassed);
+            break;
+        case 'movie-this':
             //code
             break;
-        case movie-this:
-            //code
-            break;
-        case do-what-it-says:
+        case 'do-what-it-says':
             //code
             break;
         default:
@@ -49,4 +61,8 @@ var doIt = function () {
     }
 }
 
-
+//what is the user asking for? which case, what data?
+var getCommand = function(arg3, arg4) {
+    doIt(arg3, arg4);
+}
+getCommand(process.argv[3], process.argv[4]);
